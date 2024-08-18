5345b6c34b4b46f109971207f54d086d58121036
Tars84 <gabdullintimur1984@yndex.ru>
2024-08-18 08:52:46 +0300

    CreateMainClass

---
+++ src/main/java/Order/Ingredients.java
@@ -0,0 +1,12 @@
+package Order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Ingredients {
+    private String[] ingredients;
+}
